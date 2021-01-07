3cfc19a81e9a7b8a597839c1a61a9d6b72581ce3
Alex <al851sta@htwg-konstanz.de>
2021-01-07 17:58:52 +0100

    Aufgabe 2 Teil 2

--- src/test/java/com/mycompany/app/aufgabe5/VendingMachineImplTest.java
+++ src/test/java/com/mycompany/app/aufgabe5/VendingMachineImplTest.java
@@ -1,18 +1,31 @@
 package com.mycompany.app.aufgabe5;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class VendingMachineImplTest {
+    private VendingMachineImpl vendingMachine;
+    private FullBox fullBox1;
+    private FullBox fullBox2;
+    private UnlimitedCashBox unlimitedCashBox;
 
+    @Before
+    public void setUp() {
+        unlimitedCashBox = mock(UnlimitedCashBox.class);
+        fullBox1 = mock(FullBox.class);
+        fullBox2 = mock(FullBox.class);
+        Box[] boxArray = {fullBox1, fullBox2};
+        vendingMachine = new VendingMachineImpl(unlimitedCashBox, boxArray);
+    }
     @Test
     public void testEnoughMoney() throws BoxEmptyException, NotEnoughMoneyException {
-        UnlimitedCashBox unlimitedCashBox = new UnlimitedCashBox();
-        FullBox fullBox1 = new FullBox();
-        FullBox fullBox2 = new FullBox();
-        Box[] boxArray = {fullBox1, fullBox2};
-        VendingMachineImpl vendingMachine = new VendingMachineImpl(unlimitedCashBox, boxArray);
-
+        when(fullBox2.getPrice()).thenReturn(1);
+        when(unlimitedCashBox.getCurrentAmount()).thenReturn(Integer.MAX_VALUE);
         vendingMachine.selectItem(1);
+
+        verify(fullBox2).getPrice();
+        verify(unlimitedCashBox).getCurrentAmount();
 
     }
 }
