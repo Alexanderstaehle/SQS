0cf4979bf3b11ef535a74c2bc681457075b6cece
Alex <al851sta@htwg-konstanz.de>
2021-01-07 17:59:23 +0100

    Aufgabe 2 Teil 3

--- src/test/java/com/mycompany/app/aufgabe5/VendingMachineImplTest.java
+++ src/test/java/com/mycompany/app/aufgabe5/VendingMachineImplTest.java
@@ -12,16 +12,14 @@ public class VendingMachineImplTest {
 
     @Before
     public void setUp() {
-        unlimitedCashBox = mock(UnlimitedCashBox.class);
-        fullBox1 = mock(FullBox.class);
-        fullBox2 = mock(FullBox.class);
+        unlimitedCashBox = spy(UnlimitedCashBox.class);
+        fullBox1 = spy(FullBox.class);
+        fullBox2 = spy(FullBox.class);
         Box[] boxArray = {fullBox1, fullBox2};
         vendingMachine = new VendingMachineImpl(unlimitedCashBox, boxArray);
     }
     @Test
     public void testEnoughMoney() throws BoxEmptyException, NotEnoughMoneyException {
-        when(fullBox2.getPrice()).thenReturn(1);
-        when(unlimitedCashBox.getCurrentAmount()).thenReturn(Integer.MAX_VALUE);
         vendingMachine.selectItem(1);
 
         verify(fullBox2).getPrice();
